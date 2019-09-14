5809ff9c853046885e738e09dc829359d97967e2
Aaron Abraham <aaronabraham311@gmail.com>
2019-09-14 08:58:52 -0400

    Writing basic information about app

--- app/app.py
+++ app/app.py
@@ -1,6 +1,15 @@
 # Libraries
 from flask import Flask, request, render_template, url_for, redirect
 
+# Variables
+USER_XCOORDINATE = ""
+USER_YCOORDINATE = ""
+USER_ID = ""
+
+# Gets recommendation from algorithm 
+def get_recommendations(transaction_information):
+    return transaction_information
+
 # Instantiating app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "YnJRcS9HWjY+DZoFHtE"
